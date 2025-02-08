dfb49e9e804d37594cf574853f935f0d5ce648d9
Ali İhsan Yüksel <alihsanyuksel@gmail.com>
2025-02-08 09:01:52 +0300

    matplotlib plt.hist

--- magic_data_example/magic_example.py
+++ magic_data_example/magic_example.py
@@ -11,3 +11,17 @@ print(df.head())
 # print(df.ifo())
 # print(df.describe())
 # df.to_csv("output.csv", index=False)
+
+# print(df["class"].unique())
+df["class"] = (df["class"] == "g").astype(int)
+
+print(df.head())
+
+for label in cols[:-1]:
+    plt.hist(df[df["class"] == 1][label], color='blue', label='gamma', alpha=0.7, density=True)
+    plt.hist(df[df["class"] == 0][label], color='red', label='hadron', alpha=0.7, density=True)
+    plt.title(label)
+    plt.ylabel("Probability")
+    plt.xlabel(label)
+    plt.legend()
+    plt.show()
