0d845382ac5130841cb526567ac590eaa4e808ce
ali ihsan yüksel <alihsanyuksel@gmail.com>
2025-02-08 22:11:39 +0300

    train, validation, test datasets and sklearn Standard Scaler(z-score)

--- magic_data_example/magic_example.py
+++ magic_data_example/magic_example.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 # Load the dataset
 cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
@@ -25,3 +26,18 @@ for label in cols[:-1]:
     plt.xlabel(label)
     plt.legend()
     plt.show()
+
+#train, validation, test data sets
+
+train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
+
+def scale_dataset(dataframe):
+    X = dataframe[dataframe.cols[:-1]].values
+    y = dataframe[dataframe.cols[-1]].values
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    data = np.hstack((X, np.reshape(y, (-1, 1))))
+
+    return data, X, y
