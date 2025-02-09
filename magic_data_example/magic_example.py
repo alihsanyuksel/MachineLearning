0cd5a74b637b26c3f4e2bf30c44db33d95a28bd6
ali ihsan yüksel <alihsanyuksel@gmail.com>
2025-02-09 23:12:12 +0300

    imbalanced-learn RandomOverSampler, train -> traind_df to clear coding, formatted data completed

--- magic_data_example/magic_example.py
+++ magic_data_example/magic_example.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
 
 # Load the dataset
 cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
@@ -18,26 +19,46 @@ df["class"] = (df["class"] == "g").astype(int)
 
 print(df.head())
 
-for label in cols[:-1]:
+#histograms for features
+""" for label in cols[:-1]:
     plt.hist(df[df["class"] == 1][label], color='blue', label='gamma', alpha=0.7, density=True)
     plt.hist(df[df["class"] == 0][label], color='red', label='hadron', alpha=0.7, density=True)
     plt.title(label)
     plt.ylabel("Probability")
     plt.xlabel(label)
     plt.legend()
-    plt.show()
+    plt.show() """
 
 #train, validation, test data sets
 
-train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
+train_df, valid_df, test_df = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
 
-def scale_dataset(dataframe):
-    X = dataframe[dataframe.cols[:-1]].values
-    y = dataframe[dataframe.cols[-1]].values
+def scale_dataset(dataframe, oversample=False):
+    X = dataframe[dataframe.columns[:-1]].values
+    y = dataframe[dataframe.columns[-1]].values
 
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
 
+    if oversample:
+        ros = RandomOverSampler()
+        X, y = ros.fit_resample(X, y)
+
     data = np.hstack((X, np.reshape(y, (-1, 1))))
 
     return data, X, y
+print("train")
+print(train_df)
+
+print(len(train_df[train_df["class"]==1])) #gamma
+print(len(train_df[train_df["class"]==0])) #hadron
+
+train, X_train, y_train = scale_dataset(train_df, oversample=True)
+valid, X_valid, y_valid = scale_dataset(valid_df, oversample=False)
+test, X_test, y_test = scale_dataset(test_df, oversample=False)
+
+print(len(y_train))
+
+print(sum(y_train == 1))
+print(sum(y_train == 0))
+
